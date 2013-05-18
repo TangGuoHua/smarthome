77e2790b66a371e7089b9eae2ce778fe14e964b9
hchanghao <hchanghao@9d398db2-b078-4635-8e41-45a65624efd3>
2013-05-18 03:22:18 +0000

    增加异常处理

--- trunk/Pi/smartHome/dnsget/dnsget.py
+++ trunk/Pi/smartHome/dnsget/dnsget.py
@@ -3,8 +3,11 @@ import urllib,smtplib
 from email.mime.text import MIMEText
 
 def getMyIp():
-    return urllib.urlopen("http://www.instrument.com.cn/ip.aspx").read()
-    #return urllib.urlopen("http://myip.dnsdynamic.org/").read()
+    try:
+        return urllib.urlopen("http://www.instrument.com.cn/ip.aspx").read()
+        #return urllib.urlopen("http://myip.dnsdynamic.org/").read()
+    except Exception, e:
+        return "error"
 
 def sendEmail(ipAddr, result):
     sender = 'hchanghao@163.com'
@@ -14,7 +17,7 @@ def sendEmail(ipAddr, result):
     username = 'hchanghao@163.com'
     password = '3303272q'
     try:
-        msg = MIMEText("<html>The new IP is <a href='" + ipAddr + ":9033'>" + ipAddr + ":9033</a><br><br>The result is [" + result + "].<html>", "html", "utf-8")
+        msg = MIMEText("<html>The result is [" + result + "].<br><br>The new IP is <a href='" + ipAddr + ":9033'>" + ipAddr + ":9033</a></html>", "html", "utf-8")
         msg['Subject'] = subject
         smtp = smtplib.SMTP()
         smtp.connect(smtpserver)
@@ -28,8 +31,14 @@ def sendEmail(ipAddr, result):
         return False
 
 myIp=getMyIp()
-#result=urllib.urlopen("https://hchanghao@163.com:3303272q@www.dnsdynamic.org/api/?hostname=hao.dnsget.org&myip=" + myIp).read()
-result=urllib.urlopen("http://hchanghao:3303272@dynupdate.no-ip.com/nic/update?hostname=hao.myftp.org&myip=" + myIp).read()
+if "error" in myIp:
+    result = "Error happened when retrieving my IP."
+else:
+    try:
+        result=urllib.urlopen("http://hchanghao:3303272@dynupdate.no-ip.com/nic/update?hostname=hao.myftp.org&myip=" + myIp).read()
+	#result=urllib.urlopen("https://hchanghao@163.com:3303272q@www.dnsdynamic.org/api/?hostname=hao.dnsget.org&myip=" + myIp).read()
+    except Exception, e:
+        result="Error happened when updating IP address."
 
 #print result
 #print "good" in result
