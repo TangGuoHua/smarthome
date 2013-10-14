79d325c6ea34f165aeaa5eb531dc89d5e9001a83
hchanghao <hchanghao@9d398db2-b078-4635-8e41-45a65624efd3>
2013-10-14 17:13:18 +0000

    1，增加光敏电阻 2，增加继电器2 3，继电器1和2均实现模式控制，增加自动模式 4，增加DS18B20

--- MCU/新房/02厨房_22_灯控温度/source/main.c
+++ MCU/新房/02厨房_22_灯控温度/source/main.c
@@ -5,7 +5,7 @@
 其它：
 人体红外PIR探头
 继电器模块
-
+DS18B20温度探头
 
 【修改历史】
 
@@ -22,22 +22,275 @@ Copyright(C) All Rights Reserved by Changhao Huang (HuangChangHao@gmail.com)
 ****************************************************************************/
 
 #include <reg52.h>
-#include <intrins.h>
+#include "nrf24L01Node.h"
+#include "adc.h"
+#include "stcEEPROM.h"
+#include "ds18B20.h"
 
-//红外接收头
-sbit PIR=P1^3;
 
-sbit RELAY1=P1^5;
-sbit RELAY2=P1^4;
+sfr AUXR   = 0x8E;
 
-void main(void)
+//sbit LDR = P1^7;  //光敏电阻 （10K上拉）
+sbit PIR = P1^3;  //热释电红外传感器
+
+sbit RELAY_LIGHT1 = P1^5; //灯控继电器1
+sbit RELAY_LIGHT2 = P1^4; //灯控继电器2
+
+
+unsigned char light1Mode = 2; //0：常关，1：常开，2：自动
+unsigned char light2Mode = 2; //0：常关，1：常开，2：自动
+unsigned char lightOnThreshold = 60; // 开灯的阈值
+
+
+unsigned char timerCounter10ms = 0;
+unsigned int timerSendData = 0;
+
+// Flag for sending data to Pi
+bit sendDataNow = 0;
+
+
+
+void initINT0(void)
 {
-	RELAY1=1;
-	RELAY2=1;
+	EA=1;
+	EX0=1; // Enable int0 interrupt.
+}
+
+
+
+
+//NRF24L01开始进入接收模式
+void startRecv()
+{
+	unsigned char myAddr[3]= {97, 83, 22}; //本节点的接收地址
+	nrfSetRxMode( 92, 3, myAddr); //接收92频道，3字节地址
+}
+
+
+//返回当前亮度值
+unsigned char getBrightness()
+{
+	return (255-getADCResult(7));
+}
+
+
+
+//将开灯阈值存入片内eeprom
+void saveLightOnThreshold( unsigned char x )
+{
+	eepromEraseSector( 0x0000 );
+	eepromWrite( 0x0000, x );
+}
+
+//从片内eeprom取得开灯阈值
+unsigned char getLightOnThreshold()
+{
+	return eepromRead(0x0000);
+}
+
+
+void sendDataToHost( )
+{
+	unsigned char sendData[16];
+	unsigned char toAddr[3]= {53, 69, 149}; //Pi, 96频道，3字节地址，接收16字节
+	unsigned char tmp;
+	float temperature;
+	int intTemperature;
+		
+	sendData[0]= 22;//Node ID
+
+	sendData[1] = PIR;
+	sendData[2] = getBrightness(); //亮度
+	sendData[3] = lightOnThreshold; 
+		
+	sendData[4] = ~RELAY_LIGHT1;
+	sendData[5] = ~RELAY_LIGHT2;
+
+	// DS18B20取得温度值
+	temperature = readTemperature(1);
+
+	//四舍五入到十分位
+	intTemperature = temperature<0? (temperature*(-10)+0.5) : temperature*10+0.5; 
+	
+	sendData[6]= temperature<0 ? 1 : 0; //如果是零下则此位置1
+	sendData[7]= intTemperature/10; //温度的整数部分
+	sendData[8]= intTemperature%10; //温度的小数部分
+
+	tmp = nrfSendData( 96, 3, toAddr, 16, sendData);//向Pi发送, 96频道，3字节地址，16字节数据
+	
+	//24L01开始接收数据
+	startRecv(); 
+}
+
+
+// 初始化Timer0
+void initTimer0(void)
+{
+    TMOD = 0x01;
+    TH0 = 0x63;
+    TL0 = 0xC0;
+    EA = 1;
+    ET0 = 1;
+    TR0 = 1;
+}
+
+void initRelays()
+{
+	RELAY_LIGHT1=1;
+	RELAY_LIGHT2=1;
+}
+
+void main()
+{
+
+	bit lastPIR=0;
+	bit thisPIR=0;
+	
+	AUXR = AUXR|0x80;  // T0, 1T Mode
+	
+	//初始化中断0
+	initINT0();
+    
+	//初始化继电器
+	initRelays();
+	
+	//初始化DS18B20
+	initDS18B20();
+	
+	//初始化ADC
+	initADC();
+	
+	//取得开灯阈值
+	lightOnThreshold = getLightOnThreshold();
+
+	//初始化24L01
+	nrf24L01Init();
+	
+	//24L01开始接收数据
+	startRecv(); 
+	
+	//初始化timer0
+	initTimer0();
+	
+	
 	
 	while(1)
 	{
-		RELAY1=~PIR;
-		RELAY2=~PIR;
+		//取得传感器的当前值
+		thisPIR = PIR;
+
+		//Send data to Pi
+		if( sendDataNow )
+		{
+			sendDataNow = 0;
+			sendDataToHost();
+		}
+		
+		// 灯控1
+		if( light1Mode == 0 ) //常关
+		{
+			RELAY_LIGHT1=1;
+		}
+		else if( light1Mode ==1 ) //常开
+		{
+			RELAY_LIGHT1=0;
+		}
+		else if( light1Mode ==2 ) //自动
+		{
+			if(RELAY_LIGHT1) //当前灯是灭的
+			{
+				if( thisPIR && getBrightness()<=lightOnThreshold ) //有人，且环境亮度在阈值以下
+				{
+					RELAY_LIGHT1 = 0; //开灯
+				}
+				
+			}
+			else //当前灯是亮的
+			{
+				RELAY_LIGHT1 = ~thisPIR; //继续亮，或关灯
+			}
+		}
+		
+		// 灯控2
+		if( light2Mode == 0 ) //常关
+		{
+			RELAY_LIGHT2=1;
+		}
+		else if( light2Mode ==1 ) //常开
+		{
+			RELAY_LIGHT2=0;
+		}
+		else if( light2Mode ==2 ) //自动
+		{
+			if(RELAY_LIGHT2) //当前灯是灭的
+			{
+				if( thisPIR && getBrightness()<=lightOnThreshold ) //有人，且环境亮度在阈值以下
+				{
+					RELAY_LIGHT2 = 0; //开灯
+				}
+				
+			}
+			else //当前灯是亮的
+			{
+				RELAY_LIGHT2 = ~thisPIR; //继续亮，或关灯
+			}
+		}		
+		
+		//记录当前值
+		lastPIR = thisPIR;
+
+	}
+}
+
+
+//外部中断0处理程序
+//用于处理来自24L01的中断
+void interrupt24L01(void) interrupt 0
+{
+	unsigned char * receivedData;
+	unsigned char tmp;
+	
+	//获取接收到的数据
+	receivedData = nrfGetReceivedData();
+	
+	// *(receivedData+0): 固定为1
+	// *(receivedData+1): 开灯的亮度阈值
+	// *(receivedData+2): 灯控继电器1的工作模式
+	// *(receivedData+3): 灯控继电器2的工作模式
+
+	
+	//开灯阈值
+	tmp = *(receivedData+1);
+	if( tmp!= lightOnThreshold )
+	{
+		//threshold changed, let's save it.
+		lightOnThreshold = tmp;
+		saveLightOnThreshold( lightOnThreshold );
+	}
+	
+	light1Mode = *(receivedData+2);
+	light2Mode = *(receivedData+3);
+	
+
+}
+
+
+//定时器0中断处理程序
+void timer0Interrupt(void) interrupt 1
+{
+    TH0 = 0x63;
+    TL0 = 0xC0;
+    
+	if( ++timerCounter10ms == 100 ) //100个10ms，即1秒
+	{
+		timerCounter10ms=0;
+		
+		if( ++timerSendData == 300 ) //每300秒向Pi发送一次数据
+		{
+			timerSendData = 0;
+			sendDataNow = 1;
+		}
+		
+
 	}
 }
