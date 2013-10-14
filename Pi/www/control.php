323da8a5e8cc46da4a5a677a4178f75197ceda21
hchanghao <hchanghao@9d398db2-b078-4635-8e41-45a65624efd3>
2013-10-14 14:03:35 +0000

    added Study Room Lamp

--- Pi/www/control.php
+++ Pi/www/control.php
@@ -38,7 +38,7 @@ $(document).ready(function() {
 	//隐藏设定按钮
 	showButton( "btnSetBathroomLightOnThreshold", false );
 	//showButton( "btnSetSecondaryBedTemp", false );
-	//showButton( 'btnSetAdjLamp', false );
+	showButton( "btnSetStudyroomLamp", false );
 });
 
 
@@ -71,6 +71,27 @@ function rdoBathroomHeaterClicked( val )
 }
 
 
+// study room lamp
+
+function rdoStudyRoomDeskLampClicked(val)
+{
+	//alert(val);
+	$.post("/api/sendData.php", { nodeID: "201", data1: val } );
+}
+
+function btnSetStudyroomLampClicked()
+{
+	var setVal = document.getElementById('rangeStudyroomLamp').value;
+
+	if( setVal>=0 && setVal <=255 )
+	{
+		$.post("/api/sendData.php", { nodeID: "201", data1:setVal } );
+	}
+
+	showButton( 'btnSetStudyroomLamp', false );
+}
+
+// 测试移动节点
 function rdoTestMobileLightClicked( val )
 {
 	$.post("/api/sendData.php", { nodeID: "202", data2: val } );
@@ -118,6 +139,31 @@ while ($row = $results->fetchArray())
 					<input type="radio" name="rdoBathroomHeater" id="rdoBathroomHeater1" value="1" onclick="rdoBathroomHeaterClicked('1');" <?php echo $row["fldData5"]==1?"checked":"";?> />
 					<label for="rdoBathroomHeater1">开</label>
 			</fieldset>
+		</li>
+	<?php
+	}
+	elseif( $row["fldNodeID"]==201)
+	{
+	?>
+		<li data-role="fieldcontain">
+			<fieldset data-role="controlgroup" data-type="horizontal">
+				<legend>书房台灯</legend>
+					<input type="radio" name="radioLEDGreen" id="radioLEDGreen0" value="0" onclick="rdoStudyRoomDeskLampClicked(0);" <?php echo $row["fldData1"]==0?"checked":"";?> />
+					<label for="radioLEDGreen0">关</label>
+					<input type="radio" name="radioLEDGreen" id="radioLEDGreen3" value="3" onclick="rdoStudyRoomDeskLampClicked(3);" <?php echo $row["fldData1"]==3?"checked":"";?> />
+					<label for="radioLEDGreen3">弱</label>
+					<input type="radio" name="radioLEDGreen" id="radioLEDGreen6" value="6" onclick="rdoStudyRoomDeskLampClicked(6);" <?php echo $row["fldData1"]==6?"checked":"";?> />
+					<label for="radioLEDGreen6">中</label>
+					<input type="radio" name="radioLEDGreen" id="radioLEDGreen10" value="10" onclick="rdoStudyRoomDeskLampClicked(10);" <?php echo $row["fldData1"]==10?"checked":"";?> />
+					<label for="radioLEDGreen10">强</label>
+					<input type="radio" name="radioLEDGreen" id="radioLEDGreen20" value="20" onclick="rdoStudyRoomDeskLampClicked(20);" <?php echo $row["fldData1"]==20?"checked":"";?> />
+					<label for="radioLEDGreen20">延时关</label>
+			</fieldset>
+		</li>
+		<li data-role="fieldcontain">
+			<label for="rangeStudyroomLamp">书房台灯</label>
+			<input type="range" name="rangeStudyroomLamp" id="rangeStudyroomLamp" onchange="showButton( 'btnSetStudyroomLamp', true );" value="<?php echo $row["fldData1"];?>" min="0" max="10" step="1" data-highlight="true" />
+			<button id="btnSetStudyroomLamp" data-icon="check" onclick="btnSetStudyroomLampClicked();">设定</button>
 		</li>
 	<?php
 	}
