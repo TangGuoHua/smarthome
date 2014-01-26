89a3b4f684c551366d53c52ec11b3ad31e145fa4
hchanghao <hchanghao@9d398db2-b078-4635-8e41-45a65624efd3>
2014-01-26 12:25:34 +0000

    增加温湿度的实时显示 修正Chart显示

--- trunk/Pi/www/realtimeStatus.php
+++ trunk/Pi/www/realtimeStatus.php
@@ -12,13 +12,23 @@ include "include/utils.php";
 
 $db = new DBHelper();
 
+
+$query = "select * from tabDataRecved where fldNodeID=54 order by fldID desc limit 1";
+$results = $db->query($query);
+if ($row = $results->fetchArray()) 
+{
+	echo "<li data-role='list-divider'>客厅</li>";
+	printf( "<li><a href='chart.php?param=54'>温度 %s%d.%d度 <span class='ui-li-count'>%s</span></a></li>", $row["fldData6"]==1?"-":"", $row["fldData4"], $row["fldData5"], $row["fldCreatedOn"]);
+	printf( "<li><a href='chart.php?param=54'>湿度 %d.%d%% <span class='ui-li-count'>%s</span></a></li>", $row["fldData2"], $row["fldData3"], $row["fldCreatedOn"]);
+}
+
 $query = "select * from tabDataRecved where fldNodeID=22 order by fldID desc limit 1";
 $results = $db->query($query);
 if ($row = $results->fetchArray()) 
 {
 	echo "<li data-role='list-divider'>厨房</li>";
-	printf( "<li><a href='chart.php?param=7-1'>亮度 %d <span class='ui-li-count'>%s</span></a></li>", $row["fldData2"], $row["fldCreatedOn"]);
-	printf( "<li><a href='chart.php?param=7-1'>温度 %s%d.%d度 <span class='ui-li-count'>%s</span></a></li>", $row["fldData6"]==1?"-":"", $row["fldData7"], $row["fldData8"], $row["fldCreatedOn"]);
+	printf( "<li><a href='chart.php?param=22'>亮度 %d <span class='ui-li-count'>%s</span></a></li>", $row["fldData2"], $row["fldCreatedOn"]);
+	printf( "<li><a href='chart.php?param=22'>温度 %s%d.%d度 <span class='ui-li-count'>%s</span></a></li>", $row["fldData6"]==1?"-":"", $row["fldData7"], $row["fldData8"], $row["fldCreatedOn"]);
 }
 
 
