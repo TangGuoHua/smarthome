876991be9122efa8ad7818e403725da5e1061600
hchanghao <hchanghao@9d398db2-b078-4635-8e41-45a65624efd3>
2014-09-23 15:23:33 +0000

    修改nrf24l01+驱动，以使得带PA的模块可以收到Ack 增加tabDataSent 修改log.php样式

--- Pi/www/log.php
+++ Pi/www/log.php
@@ -3,14 +3,21 @@
 <title>智能家</title>
 <meta http-equiv="content-type" content="text/html; charset=utf-8"></meta>
 <style type="text/css">
-body { width:1200px; margin:auto;  }
-a { font-size: 80pt; }
-
+body { width:1200px; margin:auto; font-family:Arial; font-size: 9px; }
+table{ border-collapse:collapse;  border-spacing:0;  }
+td {font-family:Arial; font-size: 9px; padding: 3; }
 </style>
 </head>
 <body>
 
 <?php
+
+/*
+日期        作者    备注
+------------------------------------------
+2014-SEP-23 黄长浩  增加tabDataSent，并修改一些页面样式
+*/
+
 //phpinfo( );
 
 /**
@@ -55,14 +62,31 @@ $results = $db->query($query);
 
 echo("tabDataRecved<br>" );
 echo("<table border='1'>");
-echo( "<tr><td>ID</td><td>NodeID</td><td>DATA1</td><td>DATA2</td><td>DATA3</td><td>DATA4</td><td>DATA5</td><td>DATA6</td><td>DATA7</td><td>DATA8</td><td>DATA9</td><td>DATA10</td><td>DATA11</td><td>DATA12</td><td>DATA13</td><td>DATA14</td><td>DATA15</td><td>Created On</td></tr>");
+echo( "<tr><td>ID</td><td>NodeID</td><td>D1</td><td>D2</td><td>D3</td><td>D4</td><td>D5</td><td>D6</td><td>D7</td><td>D8</td><td>D9</td><td>D10</td><td>D11</td><td>D12</td><td>D13</td><td>D14</td><td>D15</td><td>Created On</td></tr>");
 while ($row = $results->fetchArray()) {  
     //var_dump($row);
 	//echo( $row[0] );
 	//echo( "<br>");
 	printf( "<tr><td>%d</td><td>%d</td><td>%d</td><td>%d</td><td>%d</td><td>%d</td><td>%d</td><td>%d</td><td>%d</td><td>%d</td><td>%d</td><td>%d</td><td>%d</td><td>%d</td><td>%d</td><td>%d</td><td>%d</td><td>%s</td></tr>", $row["fldID"], $row["fldNodeID"], $row["fldData1"], $row["fldData2"], $row["fldData3"], $row["fldData4"], $row["fldData5"], $row["fldData6"], $row["fldData7"], $row["fldData8"], $row["fldData9"], $row["fldData10"], $row["fldData11"], $row["fldData12"], $row["fldData13"], $row["fldData14"], $row["fldData15"], $row["fldCreatedOn"]);
 } 
+
+echo ("</table><br><br>");
+
+
+$query = "select * from tabDataSent order by fldID desc limit 20";
+$results = $db->query($query);
+
+echo("tabDataSent<br>" );
+echo("<table border='1'>");
+echo( "<tr><td>ID</td><td>ToNodeID</td><td>Channel</td><td>RFPower</td><td>MaxRetry</td><td>Addr1</td><td>Addr2</td><td>Addr3</td><td>Len</td><td>D1</td><td>D2</td><td>D3</td><td>D4</td><td>D5</td><td>D6</td><td>Sent On</td><td>SentResult</td></tr>");
+while ($row = $results->fetchArray()) {  
+    //var_dump($row);
+	//echo( $row[0] );
+	//echo( "<br>");
+	printf( "<tr><td>%d</td><td>%d</td><td>%d</td><td>%d</td><td>%d</td><td>%d</td><td>%d</td><td>%d</td><td>%d</td><td>%d</td><td>%d</td><td>%d</td><td>%d</td><td>%d</td><td>%d</td><td>%s</td><td>%d</td></tr>", $row["fldID"], $row["fldToNodeID"], $row["fldRFChannel"], $row["fldRFPower"], $row["fldMaxRetry"], $row["fldAddr1"], $row["fldAddr2"], $row["fldAddr3"], $row["fldDataLength"], $row["fldData1"], $row["fldData2"], $row["fldData3"], $row["fldData4"], $row["fldData5"], $row["fldData6"],  $row["fldSentOn"], $row["fldSentResult"]);
+} 
 echo ("</table>");
+
 
 $db->close();
 ?>
